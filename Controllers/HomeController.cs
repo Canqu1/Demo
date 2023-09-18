d7aa8b1e9621062285204d3ca54653a29cdadfd9
admin <admin@DESKTOP-TFV85RN>
2023-09-18 21:36:10 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MySaleDbContext context = new MySaleDbContext();
+        public IActionResult Index()
+        {
+            List<Category> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        [HttpPost]
+        public IActionResult Process(int cateID,string cateName,string action)
+        {
+            if (action.Equals("Add"))
+            {
+                Category category = new Category()
+                {
+                    CategoryName = cateName,
+                };
+                context.Categories.Add(category);
+            }
+            else
+            {
+                Category? category = context.Categories.Where(n => n.CategoryId == cateID).FirstOrDefault();
+                if (category != null)
+                {
+                    category.CategoryName = cateName;
+                }
+            }
+            context.SaveChanges();
+            return RedirectToAction("");
+        }
+        public IActionResult Delete(int id)
+        {
+            Category? category = context.Categories.Where(n => n.CategoryId == id).Include(n => n.Products).FirstOrDefault();
+            if(category != null)
+            {
+                foreach (var item in category.Products)
+                {
+                    context.Remove(item);
+                }
+                context.Remove(category);
+            }
+            context.SaveChanges();
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ Models/MySaleDbContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Models;
+
+public partial class MySaleDbContext : DbContext
+{
+    public MySaleDbContext()
+    {
+    }
+
+    public MySaleDbContext(DbContextOptions<MySaleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("server=(local); database = MySaleDB;uid=sa;pwd=123456;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.Property(e => e.Birthdate).HasColumnType("smalldatetime");
+            entity.Property(e => e.CustomerName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName).HasMaxLength(50);
+            entity.Property(e => e.UnitPrice).HasColumnType("money");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Products_Categories");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Account);
+
+            entity.Property(e => e.Account)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Name).HasMaxLength(20);
+            entity.Property(e => e.Password)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
